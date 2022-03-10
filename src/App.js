63c0549c3383c82147c7a81cd28b81b3ba7e1693
platon-projects <vf.logvinenko@gmail.com>
2022-03-10 11:13:32 +0200

    start working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './Todo/TodoList';
+import TodoList from './Todo/TodoList';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>New app</h1>
+      <TodoList/>
     </div>
   );
 }
